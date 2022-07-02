0126e175166d807b4ed203ae0e5fc8dfd689397a
dlask913 <79985588+dlask913@users.noreply.github.com>
2022-07-02 15:49:12 +0900

    [Silver IV] Title: 보물, Time: 116 ms, Memory: 113248 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.py
@@ -0,0 +1,10 @@
+n = int(input())
+A = list(map(int,input().split()))
+B = list(map(int,input().split()))
+
+A.sort()
+B.sort(reverse=True)
+s = 0
+for i in range(n):
+    s += (A[i]*B[i])
+print(s)
