2dfbcd95b0baf63f0985a4819a6635d0c562616e
dlask913 <79985588+dlask913@users.noreply.github.com>
2022-10-02 22:41:44 +0900

    [Silver III] Title: N과 M (2), Time: 156 ms, Memory: 115720 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,19 @@
+n,m=map(int,input().split())
+lst = [0 for _ in range(n+1)]
+v=[0 for _ in range(n+1)]
+v[0]=1
+
+def NnM(lev):
+    if lev >= m:
+        if sorted(lst[:m]) == lst[:m]:
+            for i in range(m):
+                print(lst[i], end=' ')
+            print()
+        return
+    for i in range(1,n+1):
+        if v[i] == 0:
+            v[i]=1
+            lst[lev] = i
+            NnM(lev+1)
+            v[i]=0
+NnM(0)
