8ddc24cb4c8b02d77194cb6b1c58c2cd6b4ea522
dlask913 <79985588+dlask913@users.noreply.github.com>
2022-10-05 21:53:32 +0900

    [Silver III] Title: N과 M (5), Time: 2236 ms, Memory: 140280 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -0,0 +1,15 @@
+n,m=map(int,input().split())
+lst = [0 for _ in range(n+1)]
+n_value = list(map(int,input().split()))
+n_value.sort()
+def NnM(lev):
+    if lev >= m:
+        if len(set(lst[:m])) == m:
+            for i in range(m):
+                print(lst[i], end=' ')
+            print()
+        return
+    for i in n_value:
+        lst[lev] = i
+        NnM(lev+1)
+NnM(0)
