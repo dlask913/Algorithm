8cea2c74228c76ea13d0a02a3309b74a42135ba6
limnj <79985588+dlask913@users.noreply.github.com>
2023-05-06 16:04:13 +0900

    [unrated] Title: 연속된 부분 수열의 합, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/178870. 연속된 부분 수열의 합/README.md
+++ 프로그래머스/unrated/178870. 연속된 부분 수열의 합/README.md
@@ -1,10 +1,10 @@
 # [unrated] 연속된 부분 수열의 합 - 178870 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/178870) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/178870?language=python3) 
 
 ### 성능 요약
 
-메모리: 9.98 MB, 시간: 0.00 ms
+메모리: 10.1 MB, 시간: 0.00 ms
 
 ### 구분
 
--- 프로그래머스/unrated/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
+++ 프로그래머스/unrated/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
@@ -1,9 +1,7 @@
 def solution(sequence, k):
     res = []
-    
     if k in sequence:
         return [sequence.index(k),sequence.index(k)]
-    
     l,r=0,0
     s=0
     while r<len(sequence):
@@ -12,9 +10,7 @@ def solution(sequence, k):
             s-=sequence[l]
             l+=1
         if s==k:
-            if not res: 
-                res=[l,r]
-            elif (res[1]-res[0]) > (r-l):
+            if not res or (res[1]-res[0]) > (r-l): 
                 res=[l,r]
         if s>k:
             s -= sequence[r]
