4b00172cdf15903add9c7c67f6a9695dc89ee877
limnj <79985588+dlask913@users.noreply.github.com>
2023-07-12 12:56:00 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -0,0 +1,25 @@
+import heapq
+def solution(operations):
+    answer = []
+    hq = []
+    max_hq = []
+    
+    for i in operations:
+       # print(i,hq,max_hq)
+        x,y = i.split()
+        if x=='I':
+            heapq.heappush(hq,int(y))
+            heapq.heappush(max_hq,-1*int(y))
+        if x=='D' and y=='1':
+            if not max_hq: continue
+            heapq.heappop(max_hq)
+            hq = [-1*i for i in max_hq]
+            heapq.heapify(hq)
+        if x=='D' and y=='-1':
+            if not hq: continue
+            heapq.heappop(hq)
+            max_hq = [-1*i for i in hq]
+            heapq.heapify(max_hq)
+    if not hq:
+        return [0,0]
+    return [-max_hq[0],hq[0]]
