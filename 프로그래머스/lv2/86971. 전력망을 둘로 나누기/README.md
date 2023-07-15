8d65c2939e5a7091ebb6470ae6055236049dd4b7
limnj <79985588+dlask913@users.noreply.github.com>
2023-07-16 00:25:14 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 5.33 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/86971. 전력망을 둘로 나누기/README.md
+++ 프로그래머스/lv2/86971. 전력망을 둘로 나누기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전력망을 둘로 나누기 - 86971 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86971) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86971?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 3.47 ms
+메모리: 10.2 MB, 시간: 5.33 ms
 
 ### 구분
 
--- 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -7,25 +7,20 @@ def share_wire(dic, x, y):
         if i != y: q.append(i)
     cnt = 1
     while q:
-        for _ in range(len(q)):
-            node = q.popleft()
-            if v[node-1] == 0:
-                v[node-1] = 1
-                cnt += 1
-            for i in dic[node]:
-                if v[i - 1] == 0:
-                    q.append(i)
+        node = q.popleft()
+        v[node-1] = 1
+        cnt += 1
+        for i in dic[node]:
+            if v[i - 1] == 0:
+                q.append(i)
     return cnt
-
 
 def solution(n, wires):
     answer = n
     dic = {i + 1: [] for i in range(n)}
-
     for i in wires:
         dic[i[0]].append(i[1])
         dic[i[1]].append(i[0])
-
     for i in wires:
         a = share_wire(dic, i[0], i[1])
         b = share_wire(dic, i[1], i[0])
