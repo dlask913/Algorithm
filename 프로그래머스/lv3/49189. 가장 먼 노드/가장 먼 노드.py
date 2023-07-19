4bf58d2664833b75c7d43871bca407d930eb07c5
limnj <79985588+dlask913@users.noreply.github.com>
2023-07-19 20:39:41 +0900

    [level 3] Title: 가장 먼 노드, Time: 56.87 ms, Memory: 21.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -0,0 +1,18 @@
+from collections import deque
+def solution(n, edge):
+    INF = int(1e9)
+    graph = {i:[] for i in range(1,n+1)}
+    v = [INF for _ in range(n+1)]
+    v[0],v[1] = 0,0
+    for i,j in edge:
+        graph[i].append(j)
+        graph[j].append(i)
+    q = deque([(1,0)])
+    while q:
+        x,c = q.popleft()
+        for i in graph[x]:
+            if v[i] == INF:
+                v[i] = c+1
+                q.append((i,v[i]))
+    answer = v.count(max(v[1:]))
+    return answer
