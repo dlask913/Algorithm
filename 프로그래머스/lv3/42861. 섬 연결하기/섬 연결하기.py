f16d32ba84431e2bd83ea0febad7086d075c8c65
limnj <79985588+dlask913@users.noreply.github.com>
2023-07-29 14:09:25 +0900

    [level 3] Title: 섬 연결하기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.py
@@ -0,0 +1,20 @@
+import heapq
+def solution(n, costs):
+    answer = 0
+    graph = {i:[] for i in range(n)}
+    v = [0 for _ in range(n)]
+    for i,j,k in costs:
+        graph[i].append((k,j))
+        graph[j].append((k,i))
+        
+    q = [(0,0)]  
+    while q:
+        c,x = heapq.heappop(q)
+        if not v[x]:
+            v[x] = 1
+            answer += c
+            for i in graph[x]:
+                if v[i[1]] == 0:
+                    heapq.heappush(q,(i[0],i[1]))
+    
+    return answer
