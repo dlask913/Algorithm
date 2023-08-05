312c22e046169819b0d2cd67de3afb969c75c8a9
limnj <79985588+dlask913@users.noreply.github.com>
2023-08-05 15:03:04 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 16.13 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
+++ 프로그래머스/lv2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
@@ -3,25 +3,25 @@ def solution(orders, course):
     answer = []
     dic = {}
     for c in course:
-        m = 2
         tmp = []
-        for i in orders:
+        for i in orders: # orders 전체 문자열로 조합하면 시간초과
             for j in combinations(i,c):
                 tmp.append(sorted(''.join(list(j))))
         
+        m=2 # 최대 주문 수
         for i in tmp:
             cnt = 0
             for j in orders:
-                if len(set(j) & set(i)) == len(i):
+                if len(set(j) & set(i)) == len(i): # 해당 조합(i)이 있는 경우
                     cnt += 1
             if cnt>=m:
                 m = m if m > cnt else cnt
                 dic[''.join(list(i))] = cnt
+                
         for k,v in dic.items():
-            if k!=''.join(i) and len(set(k) & set(i)) == len(i) and v==m:
-                continue
             if v==m and i not in answer:
                 answer.append(k)
         dic.clear()
+        
     answer.sort()
     return answer
