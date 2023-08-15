3e54af1a1902181e692c050ccbc41dae579c4719
limnj <79985588+dlask913@users.noreply.github.com>
2023-08-15 16:12:47 +0900

    [unrated] Title: 시소 짝꿍, Time: 0.04 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/152996. 시소 짝꿍/시소 짝꿍.py
@@ -0,0 +1,21 @@
+from collections import deque,Counter
+import math
+def solution(weights):
+    answer = 0
+    weights.sort()
+    for v in Counter(weights).values():
+        if v>=2:
+            answer += sum([i for i in range(1,v)])
+    
+    dq = deque(weights)
+    tmp = []
+    for _ in range(len(weights)-1):
+        x = dq.popleft()
+        tmp.append(x*2)
+        tmp.append(x*3/2)
+        tmp.append(x*4/3)
+        
+    c = Counter(tmp)
+    for i in weights:
+        answer += c[i]
+    return answer
