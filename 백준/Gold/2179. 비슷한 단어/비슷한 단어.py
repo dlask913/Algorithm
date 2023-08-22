0872304b05d02300c807320e269d08a619b9aa8c
limnj <79985588+dlask913@users.noreply.github.com>
2023-08-22 12:37:21 +0900

    [Gold IV] Title: 비슷한 단어, Time: 200 ms, Memory: 118828 KB -BaekjoonHub

--- 백준/Gold/2179. 비슷한 단어/비슷한 단어.py
+++ 백준/Gold/2179. 비슷한 단어/비슷한 단어.py
@@ -1,32 +1,32 @@
-n= int(input())
-a = [input() for _ in range(n)]
+n = int(input())
+word = []
 
-b = sorted(list(enumerate(a)),key = lambda x: x[1])
-def check(a, b):
-    cnt = 0
-    for i in range(min(len(a), len(b))):
-        if a[i] == b[i]: cnt += 1
-        else: break
-    return cnt
-
-length = [0] * (n+1)
-maxlength = 0
-
-for i in range(n-1):
-    tmp = check(b[i][1], b[i+1][1])
-    maxlength = max(maxlength, tmp)
-    
-    length[b[i][0]] = max(length[b[i][0]], tmp)
-    length[b[i+1][0]] = max(length[b[i+1][0]], tmp)
-    
-first = 0
 for i in range(n):
-    if first == 0:
-        if length[i] == max(length):
-            first = a[i]
-            print(first)
-            pre = a[i][:maxlength]
-    else:
-        if length[i] == max(length) and a[i][:maxlength] == pre:
-            print(a[i])
-            break
+    word.append(input())
+w = sorted(list(enumerate(word)),key=lambda x:x[1])
+
+m= 0
+siz = [0 for _ in range(n)]
+for i in range(1,n):
+    cnt = 0
+    if w[i-1][1][0] == w[i][1][0]:
+        for j in range(min(len(w[i-1][1]),len(w[i][1]))):
+            if w[i-1][1][j] == w[i][1][j]:
+                cnt += 1
+            else:
+                break
+    m = max(m, cnt)
+    siz[w[i-1][0]] = max(siz[w[i-1][0]],cnt)
+    siz[w[i][0]] = max(siz[w[i][0]],cnt)
+
+flag = True
+prefix = ''
+m = max(siz)
+for k,v in enumerate(siz):
+    if v == m and flag:
+        flag = False
+        prefix = word[k][:m]
+        print(word[k])
+    elif v == m and not flag and prefix == word[k][:m]:
+        print(word[k])
+        break
