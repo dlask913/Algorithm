ccc68337a9e147cc383427233be3f160311fb49f
limnj <79985588+dlask913@users.noreply.github.com>
2023-09-08 23:20:53 +0900

    [unrated] Title: 콜라 문제, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.py
@@ -0,0 +1,10 @@
+def solution(a, b, n):
+    answer = 0
+    
+    while True:
+        if n<a:
+            break
+        answer += (n//a)*b
+        n = (n//a)*b + n%a
+        
+    return answer
