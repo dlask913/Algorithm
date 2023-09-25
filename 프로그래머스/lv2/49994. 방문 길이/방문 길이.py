d06f154e2fed3fb69a719f9d505d647e57effa36
limnj <79985588+dlask913@users.noreply.github.com>
2023-09-25 23:16:01 +0900

    [level 2] Title: 방문 길이, Time: 0.36 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/49994. 방문 길이/방문 길이.py
+++ 프로그래머스/lv2/49994. 방문 길이/방문 길이.py
@@ -2,14 +2,13 @@ def solution(dirs):
     dx = [-1,1,0,0]
     dy = [0,0,1,-1]
     dic = {"U":0,"D":1,"R":2,"L":3}
-    answer = 0
     q = []
-    x,y = 5,5
+    x,y = 0,0
     
     for i in dirs:
         d = dic[i]
         nx,ny = x+dx[d],y+dy[d]
-        if 0<=nx<=10 and 0<=ny<=10:
+        if -5<=nx<=5 and -5<=ny<=5:
             q.append((x,y,nx,ny))
             q.append((nx,ny,x,y))
             x,y = nx,ny
