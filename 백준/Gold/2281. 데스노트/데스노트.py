98c811ca531d33d27030dab597a795f7d618e704
limnj <79985588+dlask913@users.noreply.github.com>
2023-10-13 12:54:59 +0900

    Update 데스노트.py

--- 백준/Gold/2281. 데스노트/데스노트.py
+++ 백준/Gold/2281. 데스노트/데스노트.py
@@ -2,15 +2,12 @@ n, m = map(int, input().split())
 name = []
 res = float("inf")
 memo = {} # 메모이제이션을 위한 딕셔너리 생성
-def write(idx,cur,prev):
+def write(idx,cur,prev): # (name 순서, 현재 줄에 저장된 값, 지난 줄까지의 제곱 최솟값 )
     global res
-    if idx == n:
+    if idx == n: # 모든 이름을 다 적었을 때
         res = min(res, prev)
         return
-    if prev >= res:
-        return
-
-    if (idx, cur) in memo and memo[(idx, cur)] <= prev:
+    if prev >= res or (idx, cur) in memo and memo[(idx, cur)] <= prev: # 이미 계산을 했고 그 값이 현재 계산한 최솟값 보다 작으면 ( 그 이후 계산 필요X )
         return
 
     # 현재 줄에 쓸건지
@@ -19,7 +16,7 @@ def write(idx,cur,prev):
     # 다음 줄에 쓸건지
     write(idx+1,name[idx],prev+(m-cur)**2)
 
-    memo[(idx, cur)] = prev
+    memo[(idx, cur)] = prev # name[idx] 까지 적었을 때, 제곱 최솟값
 
 for _ in range(n):
     name.append(int(input()))
