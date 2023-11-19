5294e5bf728e31539bdca649235d650e0f2701e7
limnj <79985588+dlask913@users.noreply.github.com>
2023-11-19 23:51:48 +0900

    [level 1] Title: 소수 찾기, Time: 860.57 ms, Memory: 23.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
@@ -0,0 +1,12 @@
+# 에라토스테네스의 체
+def solution(n):
+    a = [False,False] + [True]*(n-1)
+    primes = []
+
+    for i in range(2,n+1):
+        if a[i]:
+            primes.append(i)
+        for j in range(2*i, n+1, i):
+            a[j] = False
+    
+    return len(primes)
