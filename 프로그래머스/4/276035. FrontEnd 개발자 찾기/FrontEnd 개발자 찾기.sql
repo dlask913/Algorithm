0cd67ac61ac2572d2e8b06112848ae809686f340
limnj <79985588+dlask913@users.noreply.github.com>
2024-06-14 12:26:33 +0900

    [level 4] Title: FrontEnd 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
@@ -0,0 +1,8 @@
+-- 코드를 작성해주세요
+SELECT ID, EMAIL, FIRST_NAME, LAST_NAME
+FROM DEVELOPERS
+WHERE (SKILL_CODE & ( SELECT SUM(CODE) 
+                      FROM SKILLCODES 
+                      WHERE CATEGORY = 'Front End')) > 0
+ORDER BY ID
+;
