d5401f708fe3e45d15e9ffaeeef5c91f49866842
limnj <79985588+dlask913@users.noreply.github.com>
2026-09-22 14:15:54 +0900

    [level 2] Title: 올바른 괄호, Time: 10.16 ms, Memory: 57.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        Stack<Character> stack = new Stack<>();
+        int n = s.length();
+
+        for(int i=0; i<n; i++){
+            Character tmp = s.charAt(i);
+            if(tmp == ')'){
+                if(stack.isEmpty() || stack.peek()==')') return false;
+                stack.pop();
+            } else {
+                stack.push('(');
+            }
+        }
+        
+        if(!stack.isEmpty()) return false;
+        
+        return answer;
+    }
+}
